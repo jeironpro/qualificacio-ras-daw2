773899d5a9fb2618a0e4955ae8cb25acfb92664d
jeironpro <jeironprogrammer@gmail.com>
2025-12-06 00:31:43 +0100

    Projecte acabat

---
+++ js/ras.js
@@ -0,0 +1,273 @@
+const moduls_ras = [
+    {
+        "m": "0612_ICC0",
+        "nom": "Desenvolupament Web en Entorn Client",
+        "resultats_aprenentatge": [
+            {
+                "ra1": "Selecciona les arquitectures i tecnologies de programació sobre clients web, identificant i analitzant les capacitats i les característiques de cadascuna.",
+                "nota": 9.35,
+                "nota_final": 9
+            },
+            {
+                "ra2": "Escriu sentències simples, aplicant la sintaxi del llenguatge i verificant la seva execució sobre navegadors web.",
+                "nota": 7.83,
+                "nota_final": 8
+            },
+            {
+                "ra3": "Escriu codi, identificant i aplicant les funcionalitats aportades pels objectes predefinits del llenguatge.",
+                "nota": "",
+                "nota_final": ""
+            },
+            {
+                "ra4": "Programa codi per a clients web analitzant i utilitzant estructures definides per l'usuari.",
+                "nota": "",
+                "nota_final": ""
+            },
+            {
+                "ra5": "Desenvolupa aplicacions web interactives integrant mecanismes de maneig d'esdeveniments.",
+                "nota": "",
+                "nota_final": ""
+            },
+            {
+                "ra6": "Desenvolupa aplicacions web analitzant i aplicant les característiques del model d'objectes del document.",
+                "nota": 8.5,
+                "nota_final": 9
+            },
+            {
+                "ra7": "Desenvolupa aplicacions web dinàmiques, reconeixent i aplicant mecanismes de comunicació asíncrona entre client i servidor.",
+                "nota": "",
+                "nota_final": ""
+            }
+        ]
+    },
+    {
+        "m": "0613_ICC0",
+        "nom": "Desenvolupament Web en Entorn Servidor",
+        "resultats_aprenentatge": [
+            {
+                "ra1": "Selecciona les arquitectures i tecnologies de programació web en entorn servidor, analitzant les capacitats i característiques pròpies.",
+                "nota": 9.35,
+                "nota_final": 9
+            },
+            {
+                "ra2": "Escriu sentències executables per un servidor web reconeixent i aplicant procediments d’integració del codi en llenguatges de marques.",
+                "nota": "",
+                "nota_final": ""
+            },
+            {
+                "ra3": "Escriu blocs de sentències embeguts en llenguatges de marques, seleccionant i utilitzant les estructures de programació.",
+                "nota": "",
+                "nota_final": ""
+            },
+            {
+                "ra4": "Desenvolupa aplicacions web embegudes en llenguatges de marques analitzant i incorporant funcionalitats segons especificacions.",
+                "nota": "",
+                "nota_final": ""
+            },
+            {
+                "ra5": "Desenvolupa aplicacions web identificant i aplicant mecanismes per separar el codi de presentació de la lògica de negoci.",
+                "nota": "",
+                "nota_final": ""
+            },
+            {
+                "ra6": "Desenvolupa aplicacions web d'accés a magatzems de dades, aplicant mesures per mantenir la seguretat i la integritat de la informació.",
+                "nota": "",
+                "nota_final": ""
+            },
+            {
+                "ra7": "Desenvolupa serveis web reutilitzables i accessibles mitjançant protocols web, verificant-ne el funcionament.",
+                "nota": "",
+                "nota_final": ""
+            },
+            {
+                "ra8": "Genera pàgines web dinàmiques analitzant i utilitzant tecnologies i frameworks del servidor web que afegeixin codi al llenguatge de marques.",
+                "nota": "",
+                "nota_final": ""
+            },
+            {
+                "ra9": "Desenvolupa aplicacions web híbrides seleccionant i utilitzant tecnologies, frameworks servidor i repositoris heterogenis d'informació.",
+                "nota": "",
+                "nota_final": ""
+            }
+        ]
+    },
+    {
+        "m": "0614_ICC0",
+        "nom": "Desplegament d'Aplicacions Web",
+        "resultats_aprenentatge": [
+            {
+                "ra1": "Implanta arquitectures web analitzant i aplicant criteris de funcionalitat.",
+                "nota": "",
+                "nota_final": ""
+            },
+            {
+                "ra2": "Implanta aplicacions web en servidors web, avaluant i aplicant criteris de configuració per al funcionament segur.",
+                "nota": "",
+                "nota_final": ""
+            },
+            {
+                "ra3": "Implanta aplicacions web en servidors d'aplicacions, avaluant i aplicant criteris de configuració per al funcionament segur.",
+                "nota": "",
+                "nota_final": ""
+            },
+            {
+                "ra4": "Administra servidors de transferència de fitxers, avaluant i aplicant criteris de configuració que garanteixin la disponibilitat del servei.",
+                "nota": "",
+                "nota_final": ""
+            },
+            {
+                "ra5": "Verifica l'execució d'aplicacions web comprovant els paràmetres de configuració de serveis de xarxa.",
+                "nota": "",
+                "nota_final": ""
+            },
+            {
+                "ra6": "Elabora la documentació de l'aplicació web avaluant i seleccionant eines de generació de documentació, control de versions i d'integració contínua.",
+                "nota": "",
+                "nota_final": ""
+            }
+        ]
+    },
+    {
+        "m": "0616_ICC0",
+        "nom": "Projecte Intermodular de Desenvolupament d'Aplicacions Web",
+        "resultats_aprenentatge": [
+            {
+                "ra1": "Projecte guiat.",
+                "nota": "",
+                "nota_final": ""
+            },
+            {
+                "ra2": "Projecte multidisciplinar amb uns requeriments mínims.",
+                "nota": "",
+                "nota_final": ""
+            }
+        ]
+    },
+    {
+        "m": "1665_ICC0",
+        "nom": "Digitalització Aplicada als Sectors Productius",
+        "resultats_aprenentatge": [
+            
+        ]
+    },
+    {
+        "m": "1708_ICC0",
+        "nom": "Sostenibilitat Aplicada al Sistema Productiu",
+        "resultats_aprenentatge": [
+            {
+                "ra1": "ASG i desenvolupament sostenible.",
+                "nota": 10,
+                "nota_final": 10
+            },
+            {
+                "ra2": "Reptes ambientals i socials.",
+                "nota": 10,
+                "nota_final": 10
+            },
+            {
+                "ra3": "Criteris de sostenibilitat en l'acompliment professional i personal.",
+                "nota": "",
+                "nota_final": ""
+            },
+            {
+                "ra4": "Productes i Serveis Responsables i Economia Circular.",
+                "nota": "",
+                "nota_final": ""
+            },
+            {
+                "ra5": "Activitats Sostenibles i Minimització de l'Impacte Mediambiental.",
+                "nota": "",
+                "nota_final": ""
+            },
+            {
+                "ra6": "Anàlisi d'un pla de sostenibilitat.",
+                "nota": "",
+                "nota_final": ""
+            }
+        ]
+    },
+    {
+        "m": "1710_ICC0",
+        "nom": "Itinerari Personal per a l'Ocupabilitat II",
+        "resultats_aprenentatge": [
+            {
+                "ra1": "Estratègies per a la cerca de feina\nPlanifica i posa en marxa estratègies en els diferents processos selectius d'ocupació que li permeten millorar les seves possibilitats d'inserció laboral.",
+                "nota": "",
+                "nota_final": ""
+            },
+            {
+                "ra2": "Competències per a l'ocupabilitat\nAplica estratègies relacionades amb les competències personals, socials i emocionals per al desenvolupament de la seva iniciativa emprenedora i la millora de la seva ocupabilitat.",
+                "nota": "",
+                "nota_final": ""
+            },
+            {
+                "ra3": "Innovació i Emprenedoria\nPosa en pràctica les habilitats emprenedores necessàries per al desenvolupament de processos d'innovació i recerca aplicades que promouen la modernització del sector productiu cap a un model sostenible.",
+                "nota": "",
+                "nota_final": ""
+            },
+            {
+                "ra4": "Idees emprenedores i Noves oportunitats\nIdentifica, defineix i valida idees d'emprenedoria generadores de noves oportunitats a partir d'estratègies d'anàlisi de l'entorn socioproductiu utilitzant metodologies àgils per l'emprenedoria.",
+                "nota": "",
+                "nota_final": ""
+            },
+            {
+                "ra5": "El Projecte emprenedor\nDesenvolupa un projecte emprenedor d'innovació social i/o tecnològica aplicada en col·laboració amb l'entorn.",
+                "nota": "",
+                "nota_final": ""
+            }
+        ]
+    },
+    {
+        "m": "machine_learning",
+        "nom": "Modul Profesional Optatiu + Machine Learning",
+        "resultats_aprenentatge": [
+            {
+                "ra1": "Crea aplicacions fent ús de models d'aprenentatge automàtic",
+                "nota": "",
+                "nota_final": ""
+            }
+        ]
+    }
+]
+
+const renderRAs = function(moduleCode, containerId) {
+    const container = document.getElementById(containerId);
+    if (!container) return;
+
+    const modul = moduls_ras.find(m => m.m === moduleCode);
+    if (!modul) return;
+
+    modul.resultats_aprenentatge.forEach((ra, index) => {
+        const contenidorRa = document.createElement("div");
+        contenidorRa.classList.add("ra");
+
+        const nombreRa = document.createElement("span");
+        nombreRa.textContent = `RA${index + 1}`;
+
+        const contingutRa = document.createElement("span");
+        contingutRa.textContent = ra[`ra${index + 1}`];
+
+        const notaRa = document.createElement("span");
+
+        const nota = ra.nota;
+        const notaFinal = ra.nota_final;
+
+        if (!nota && !notaFinal) {
+            notaRa.textContent = "Pendent";
+        } else {
+            notaRa.textContent = `${nota ?? ""} (${notaFinal ?? ""})`;
+        }
+
+        contenidorRa.append(nombreRa, contingutRa, notaRa);
+        container.appendChild(contenidorRa);
+    });
+}
+
+renderRAs("0612_ICC0", "ras_0612_icc0");
+renderRAs("0613_ICC0", "ras_0613_icc0");
+renderRAs("0614_ICC0", "ras_0614_icc0");
+renderRAs("0616_ICC0", "ras_0616_icc0");
+renderRAs("1665_ICC0", "ras_1665_icc0");
+renderRAs("1708_ICC0", "ras_1708_icc0");
+renderRAs("1710_ICC0", "ras_1710_icc0");
+renderRAs("machine_learning", "ras_machine_learning_icc0");
