423fa98575695356fb564adbd7538d703ea74a50
jeironpro <jeironprogrammer@gmail.com>
2026-01-11 01:59:12 +0100

    Mejorando formato y calculo de datos

--- js/ras.js
+++ js/ras.js
@@ -6,37 +6,37 @@ const moduls_ras = [
             {
                 "ra1": "Selecciona les arquitectures i tecnologies de programació sobre clients web, identificant i analitzant les capacitats i les característiques de cadascuna.",
                 "nota": 9.35,
-                "nota_final": 9
+                "nota_final": null
             },
             {
                 "ra2": "Escriu sentències simples, aplicant la sintaxi del llenguatge i verificant la seva execució sobre navegadors web.",
                 "nota": 7.83,
-                "nota_final": 8
+                "nota_final": null
             },
             {
                 "ra3": "Escriu codi, identificant i aplicant les funcionalitats aportades pels objectes predefinits del llenguatge.",
-                "nota": "",
-                "nota_final": ""
+                "nota": null,
+                "nota_final": null
             },
             {
                 "ra4": "Programa codi per a clients web analitzant i utilitzant estructures definides per l'usuari.",
-                "nota": "",
-                "nota_final": ""
+                "nota": null,
+                "nota_final": null
             },
             {
                 "ra5": "Desenvolupa aplicacions web interactives integrant mecanismes de maneig d'esdeveniments.",
-                "nota": "",
-                "nota_final": ""
+                "nota": null,
+                "nota_final": null
             },
             {
                 "ra6": "Desenvolupa aplicacions web analitzant i aplicant les característiques del model d'objectes del document.",
                 "nota": 8.5,
-                "nota_final": 9
+                "nota_final": null
             },
             {
                 "ra7": "Desenvolupa aplicacions web dinàmiques, reconeixent i aplicant mecanismes de comunicació asíncrona entre client i servidor.",
-                "nota": "",
-                "nota_final": ""
+                "nota": null,
+                "nota_final": null
             }
         ]
     },
@@ -47,47 +47,47 @@ const moduls_ras = [
             {
                 "ra1": "Selecciona les arquitectures i tecnologies de programació web en entorn servidor, analitzant les capacitats i característiques pròpies.",
                 "nota": 9.35,
-                "nota_final": 9
+                "nota_final": null
             },
             {
                 "ra2": "Escriu sentències executables per un servidor web reconeixent i aplicant procediments d’integració del codi en llenguatges de marques.",
-                "nota": "",
-                "nota_final": ""
+                "nota": null,
+                "nota_final": null
             },
             {
                 "ra3": "Escriu blocs de sentències embeguts en llenguatges de marques, seleccionant i utilitzant les estructures de programació.",
-                "nota": "",
-                "nota_final": ""
+                "nota": null,
+                "nota_final": null
             },
             {
                 "ra4": "Desenvolupa aplicacions web embegudes en llenguatges de marques analitzant i incorporant funcionalitats segons especificacions.",
-                "nota": "",
-                "nota_final": ""
+                "nota": null,
+                "nota_final": null
             },
             {
                 "ra5": "Desenvolupa aplicacions web identificant i aplicant mecanismes per separar el codi de presentació de la lògica de negoci.",
-                "nota": "",
-                "nota_final": ""
+                "nota": null,
+                "nota_final": null
             },
             {
                 "ra6": "Desenvolupa aplicacions web d'accés a magatzems de dades, aplicant mesures per mantenir la seguretat i la integritat de la informació.",
-                "nota": "",
-                "nota_final": ""
+                "nota": null,
+                "nota_final": null
             },
             {
                 "ra7": "Desenvolupa serveis web reutilitzables i accessibles mitjançant protocols web, verificant-ne el funcionament.",
-                "nota": "",
-                "nota_final": ""
+                "nota": null,
+                "nota_final": null
             },
             {
                 "ra8": "Genera pàgines web dinàmiques analitzant i utilitzant tecnologies i frameworks del servidor web que afegeixin codi al llenguatge de marques.",
-                "nota": "",
-                "nota_final": ""
+                "nota": null,
+                "nota_final": null
             },
             {
                 "ra9": "Desenvolupa aplicacions web híbrides seleccionant i utilitzant tecnologies, frameworks servidor i repositoris heterogenis d'informació.",
-                "nota": "",
-                "nota_final": ""
+                "nota": null,
+                "nota_final": null
             }
         ]
     },
@@ -97,33 +97,33 @@ const moduls_ras = [
         "resultats_aprenentatge": [
             {
                 "ra1": "Implanta arquitectures web analitzant i aplicant criteris de funcionalitat.",
-                "nota": "",
-                "nota_final": ""
+                "nota": null,
+                "nota_final": null
             },
             {
                 "ra2": "Implanta aplicacions web en servidors web, avaluant i aplicant criteris de configuració per al funcionament segur.",
-                "nota": "",
-                "nota_final": ""
+                "nota": null,
+                "nota_final": null
             },
             {
                 "ra3": "Implanta aplicacions web en servidors d'aplicacions, avaluant i aplicant criteris de configuració per al funcionament segur.",
-                "nota": "",
-                "nota_final": ""
+                "nota": null,
+                "nota_final": null
             },
             {
                 "ra4": "Administra servidors de transferència de fitxers, avaluant i aplicant criteris de configuració que garanteixin la disponibilitat del servei.",
-                "nota": "",
-                "nota_final": ""
+                "nota": null,
+                "nota_final": null
             },
             {
                 "ra5": "Verifica l'execució d'aplicacions web comprovant els paràmetres de configuració de serveis de xarxa.",
-                "nota": "",
-                "nota_final": ""
+                "nota": null,
+                "nota_final": null
             },
             {
                 "ra6": "Elabora la documentació de l'aplicació web avaluant i seleccionant eines de generació de documentació, control de versions i d'integració contínua.",
-                "nota": "",
-                "nota_final": ""
+                "nota": null,
+                "nota_final": null
             }
         ]
     },
@@ -133,13 +133,13 @@ const moduls_ras = [
         "resultats_aprenentatge": [
             {
                 "ra1": "Projecte guiat.",
-                "nota": "",
-                "nota_final": ""
+                "nota": null,
+                "nota_final": null
             },
             {
                 "ra2": "Projecte multidisciplinar amb uns requeriments mínims.",
-                "nota": "",
-                "nota_final": ""
+                "nota": null,
+                "nota_final": null
             }
         ]
     },
@@ -157,32 +157,32 @@ const moduls_ras = [
             {
                 "ra1": "ASG i desenvolupament sostenible.",
                 "nota": 10,
-                "nota_final": 10
+                "nota_final": null
             },
             {
                 "ra2": "Reptes ambientals i socials.",
                 "nota": 10,
-                "nota_final": 10
+                "nota_final": null
             },
             {
                 "ra3": "Criteris de sostenibilitat en l'acompliment professional i personal.",
-                "nota": "",
-                "nota_final": ""
+                "nota": null,
+                "nota_final": null
             },
             {
                 "ra4": "Productes i Serveis Responsables i Economia Circular.",
-                "nota": "",
-                "nota_final": ""
+                "nota": null,
+                "nota_final": null
             },
             {
                 "ra5": "Activitats Sostenibles i Minimització de l'Impacte Mediambiental.",
-                "nota": "",
-                "nota_final": ""
+                "nota": null,
+                "nota_final": null
             },
             {
                 "ra6": "Anàlisi d'un pla de sostenibilitat.",
-                "nota": "",
-                "nota_final": ""
+                "nota": null,
+                "nota_final": null
             }
         ]
     },
@@ -192,28 +192,28 @@ const moduls_ras = [
         "resultats_aprenentatge": [
             {
                 "ra1": "Estratègies per a la cerca de feina\nPlanifica i posa en marxa estratègies en els diferents processos selectius d'ocupació que li permeten millorar les seves possibilitats d'inserció laboral.",
-                "nota": "",
-                "nota_final": ""
+                "nota": null,
+                "nota_final": null
             },
             {
                 "ra2": "Competències per a l'ocupabilitat\nAplica estratègies relacionades amb les competències personals, socials i emocionals per al desenvolupament de la seva iniciativa emprenedora i la millora de la seva ocupabilitat.",
-                "nota": "",
-                "nota_final": ""
+                "nota": null,
+                "nota_final": null
             },
             {
                 "ra3": "Innovació i Emprenedoria\nPosa en pràctica les habilitats emprenedores necessàries per al desenvolupament de processos d'innovació i recerca aplicades que promouen la modernització del sector productiu cap a un model sostenible.",
-                "nota": "",
-                "nota_final": ""
+                "nota": null,
+                "nota_final": null
             },
             {
                 "ra4": "Idees emprenedores i Noves oportunitats\nIdentifica, defineix i valida idees d'emprenedoria generadores de noves oportunitats a partir d'estratègies d'anàlisi de l'entorn socioproductiu utilitzant metodologies àgils per l'emprenedoria.",
-                "nota": "",
-                "nota_final": ""
+                "nota": null,
+                "nota_final": null
             },
             {
                 "ra5": "El Projecte emprenedor\nDesenvolupa un projecte emprenedor d'innovació social i/o tecnològica aplicada en col·laboració amb l'entorn.",
-                "nota": "",
-                "nota_final": ""
+                "nota": null,
+                "nota_final": null
             }
         ]
     },
@@ -223,12 +223,21 @@ const moduls_ras = [
         "resultats_aprenentatge": [
             {
                 "ra1": "Crea aplicacions fent ús de models d'aprenentatge automàtic",
-                "nota": "",
-                "nota_final": ""
+                "nota": null,
+                "nota_final": null
             }
         ]
     }
 ]
+
+moduls_ras.forEach(modul => {
+    modul.resultats_aprenentatge.forEach(ra => {
+        ra.nota_final = Number.isFinite(ra.nota)
+            ? Math.round(ra.nota)
+            : null;
+    });
+});
+
 
 const renderRAs = function(moduleCode, containerId) {
     const container = document.getElementById(containerId);
